9b1a7174c497643d3b35eb84846792b4471af477
Ivana Huckova <ivana.huckova@gmail.com>
2019-04-05 10:37:59 +0200

    Create login component with login functionality

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,5 +1,42 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import axios from 'axios';
 
 export default function Login(props) {
-  return <div>Login</div>;
+  const [message, setMessage] = useState('');
+
+  const usernameRef = useRef();
+  const passwordRef = useRef();
+
+  const loginUser = e => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:3300/api/login', {
+        username: usernameRef.current.value,
+        password: passwordRef.current.value
+      })
+      .then(res => {
+        setMessage(res.data.message);
+        localStorage.setItem('token', res.data.token);
+      })
+      .catch(err => {
+        setMessage(`Error: ${err.response.data.message}`);
+      });
+  };
+
+  return (
+    <div>
+      <form onSubmit={loginUser}>
+        <div>
+          Username: <input type="text" ref={usernameRef} />
+        </div>
+        <div>
+          Password: <input type="password" ref={passwordRef} />
+        </div>
+        <div>
+          <input type="submit" value="submit" />
+        </div>
+      </form>
+      <div>{message}</div>
+    </div>
+  );
 }
