e2d99c503030532b4770fa481c8f19d813938eff
Ivana Huckova <ivana.huckova@gmail.com>
2019-04-05 09:23:54 +0200

    Create functional register

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,6 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
+const db = require('../database/dbConfig');
 
 const { authenticate } = require('../auth/authenticate');
 
@@ -8,17 +10,27 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
-function register(req, res) {
-  // implement user registration
+async function register(req, res) {
+  try {
+    let { username, password } = req.body;
+    if (username && password) {
+      let hash = bcrypt.hashSync(password, 10);
+      password = hash;
+      const newUser = await db('users').insert({ username, password });
+      res.status(201).json({ id: newUser[0], username, password });
+    } else {
+      res.status(400).json({ message: 'Make sure that username and password are included ' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 }
 
-function login(req, res) {
-  // implement user login
-}
+async function login(req, res) {}
 
 function getJokes(req, res) {
   const requestOptions = {
-    headers: { accept: 'application/json' },
+    headers: { accept: 'application/json' }
   };
 
   axios
